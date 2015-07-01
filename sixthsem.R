63a0b5d6a05695521604d5c7afa6a708467817fd
Aditya Salapaka <salapaka.aditya@gmail.com>
2015-07-01 11:05:29 +0530

    added function to extract results by branch

--- sixthsem.R
+++ sixthsem.R
@@ -42,8 +42,9 @@ length(results) # 1479
 results <- results[grep("([0-9]\\s+\\w+)", results)]
 length(results)
 
-x <- gsub("\\b[A-z]{1,2}\\b-.+", "", results)
-x <- gsub("[A-Z]+\\dK", " 2K", x) # replaces FOO BOO BAR2K12 with FOO BOO 2K12
+results <- gsub("\\b[A-z]{1,2}\\b-.+", "", results) # removes backs
+(?<=a)b
+x <- gsub("[A-Z]+2K", " 2K", results) # replaces FOO BOO BAR2K12 with FOO BOO 2K12
 
 s <- gsub("^ *|(?<= ) | *$", "", x, perl = T)
 df <- read.table(text=gsub("(?<=[[:digit:]] )(.*)(?= 2K12)", "'\\1'", s, 
@@ -52,3 +53,9 @@ df <- read.table(text=gsub("(?<=[[:digit:]] )(.*)(?= 2K12)", "'\\1'", s,
 df <- df[naturalorder(df$V3),]
 
 rownames(df) <- NULL
+
+branchResults <- function(branchCode){
+        branch <- df[grep(paste("2K12/", branchCode, sep = ""), df$V3),]
+}
+
+mechanical <- branchResults("ME")
