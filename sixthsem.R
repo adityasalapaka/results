bfb31fea6e29d3e27c4cebc6e74a65167217bd99
Aditya Salapaka <salapaka.aditya@gmail.com>
2015-07-03 13:58:07 +0530

    Began commenting the script.

--- sixthsem.R
+++ sixthsem.R
@@ -1,13 +1,30 @@
+# Sets working directory.
+
 setwd("~/results")
+
+# Loads natural sort into the environment. Uncomment the next line to install
+# it.
+
+# install.packages("naturalsort)
 
 library(naturalsort)
 
+# Function used to convert the PDF to text. Accepts a character vector with
+# the file name. Ensure that the file to be extracted is the working directorty
+# and that there are no other text files in the working directory.
+
 extract <- function(resultsPDF){
         
-        print(resultsPDF)
+        # Counts number of pages in the PDF file.
+        
         pages <- system(paste("pdfinfo ", resultsPDF, 
                               ".pdf | awk '/^Pages:/ {print $2}'", sep = ""), 
                         intern = TRUE)
+        
+        # Extracts text from each page to individual text files, preserving
+        # the original layout. This creates the first batch of text files. 
+        # Unwanted text at the head and the tail are stripped off to create 
+        # a second batch of text files.
         
         for (i in 1:pages){
                 system(paste("pdftotext ", resultsPDF, ".pdf -f ", i," -l ", i, 
@@ -16,12 +33,17 @@ extract <- function(resultsPDF){
                              "output.txt", sep = ""))
         }
         
+        # Removes the first batch of unwanted text files.
         
         for(i in 1:pages){
                 system(paste("rm ", i, ".txt", sep=""))
         }
         
+        # Creates a contiguous text file from the indivdual ones.
+        
         system("cat *.txt > results.txt")
+        
+        # Removes all remaining text files.
         
         for(i in 1:pages){
                 system(paste("rm ", i, "output.txt", sep=""))
